25e8972fb85ffedef96e9575baef68fead67e107
Dallas Hart <36043275+Nomalah@users.noreply.github.com>
2024-05-26 09:45:24 -0400

    feat: working locationfix for GPS on vehicle

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { MessageEvent } from "@foxglove/extension";
+import { LocationFix } from "@foxglove/schemas";
+import { ExtensionContext } from "@foxglove/studio";
+
+interface RLM_POSITION_0XF2 {
+  LATITUDE: number;
+  LONGITUDE: number;
+}
+
+export function activate(extensionContext: ExtensionContext): void {
+  extensionContext.registerMessageConverter({
+    fromSchemaName: "RLM_POSITION_0XF2",
+    toSchemaName: "foxglove.LocationFix",
+    converter: (
+      rlm_position_msg: RLM_POSITION_0XF2,
+      message_event: MessageEvent<RLM_POSITION_0XF2>,
+    ): Partial<LocationFix> => {
+      return {
+        latitude: rlm_position_msg.LATITUDE,
+        longitude: rlm_position_msg.LONGITUDE,
+        timestamp: message_event.receiveTime,
+      };
+    },
+  });
+}
